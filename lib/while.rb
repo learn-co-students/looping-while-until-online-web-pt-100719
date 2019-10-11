cae348212557a99b99381ca2c2d4e3a3e16fe203
Adrian Bruno <adrian.j.bruno@gmail.com>
2019-10-11 21:11:59 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,10 @@
 def using_while
-  levitation_force = 6
+  levitation_force = 6 # Declares the variable and sets its value
 	
-  #your code here
+  while levitation_force < 10 #Begins our loop, brings in the value of our variable and sets a 
+                              # condition
+    puts "Wingardium Leviosa" # Outputs the string "Wingardium Leviosa"
+    levitation_force += 1     # Adds one to the value of our variable
+  end
+  
 end
-
-
